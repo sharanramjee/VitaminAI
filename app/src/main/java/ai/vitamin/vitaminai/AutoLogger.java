8bb4ad8c97d8020e3782695f8e931bbc47e46709
Sharan Ramjee <sharanramjee@gmail.com>
2018-10-20 00:04:44 -0400

    Updated UI background and made Notif Bar transparent

---
+++ app/src/main/java/ai/vitamin/vitaminai/AutoLogger.java
@@ -0,0 +1,13 @@
+package ai.vitamin.vitaminai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutoLogger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_auto_logger);
+    }
+}
