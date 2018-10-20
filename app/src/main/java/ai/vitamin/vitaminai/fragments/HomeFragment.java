511238ba0142109e9ed4c6aaece59950d59cdb27
Matthew Wen <mattwen2018@gmail.com>
2018-10-20 02:04:37 -0400

    Added Button for activity

--- app/src/main/java/ai/vitamin/vitaminai/fragments/HomeFragment.java
+++ app/src/main/java/ai/vitamin/vitaminai/fragments/HomeFragment.java
@@ -1,11 +1,15 @@
 package ai.vitamin.vitaminai.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import ai.vitamin.vitaminai.AutoLogger;
 import ai.vitamin.vitaminai.R;
 
 public class HomeFragment extends Fragment {
@@ -26,7 +30,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        Button button = view.findViewById(R.id.to_activity_b);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), AutoLogger.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
